f21cff53e3891c88552044c240c4730c275c8086
Yichen Li <yichen.li.1@stonybrook.edu>
2023-02-26 18:48:48 -0500

    Feb26 18:48

--- src/bacon.c
+++ src/bacon.c
@@ -1,45 +1,78 @@
-//#include "bacon.h"
+#include "bacon.h"
 #include <stdio.h>
 #include <stdlib.h>
+
 
 // Add other #includes here if you want.
 // Planing to use a hashtable to transform chars to a unsigned int. 
 
 
+// change the char to ASCII values. 
 int char_to_ascii (char input_char){
-    char temp_char = '0';
-    int result = input_char - temp_char;
-    return result;
+    return input_char;
 }
 
-unsigned int hash_table_function(int ascii_from_above){
+
+
+unsigned int return_bacon_code(int ascii_from_above){
     unsigned int result = 0;
-    if ((ascii_from_above < 43) && (ascii_from_above > 16))
+    // if uppercase chars from A to Z
+    if ((ascii_from_above >= 65) && (ascii_from_above < 91))
     {
-        int temp = ascii_from_above - 17;
+        int temp = ascii_from_above - 65;
         result += (unsigned int) temp;
         return result;
     }
-    else if ((ascii_from_above < 0) && (ascii_from_above > -17))
+    //if symbols
+    else if ((ascii_from_above >= 32) && (ascii_from_above < 48))
     {
-        unsigned int temp = ascii_from_above + 16;
+        unsigned int temp = ascii_from_above - 32;
         result = (unsigned int) 26 + temp;
         return result;
     }
-
+    // if numbers from 0 to 9
+    else if ((ascii_from_above >= 48) && (ascii_from_above < 58)){
+        unsigned int temp = ascii_from_above - 48;
+        result = (unsigned int) 40 + temp;
+        return result;
+    }
+    // if : or ;
+    else if((ascii_from_above >= 58 ) && (ascii_from_above < 60)){
+        unsigned int temp = ascii_from_above - 58;
+        result = (unsigned int) 50 + temp;
+        return result;
+    }
+    // if ?
+    else if(ascii_from_above == 63) {
+        unsigned int temp = ascii_from_above - 63;
+        result = (unsigned int) 52 + temp;
+        return result;
+    }
+    // other, use to deal with unknown chars
+    else{
+        return -999;
+    }
 }
 
 
 
-int main(int argc, char const *argv[])
-{
-    printf("The int is: %d \n", char_to_ascii('A'));
-    return 0;
-}
 
 
 
 int encrypt(const char *plaintext, char *ciphertext) {
+    int length_of_cipher = strlen(ciphertext);
+    int length_of_plain = strlen(plaintext);
+    length_of_cipher *= 6;
+    // if not overflow
+    if (length_of_plain <= length_of_cipher)
+    {
+        return -1000;
+    }
+    // if overflow
+    else{
+
+    }
+    
     return -1000;
 }
 
